c8c460fb9fd0ae8a95eea7d31172b3ae6c07e4aa
Pavel Belousov <signorpao@gmail.com>
2024-04-17 08:28:33 +0300

    header navigation base functionality is done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
-// mobile menu show / close
 let menuBtn = document.querySelector(".mobile__header-burger"),
   body = document.querySelector("body"),
-  links = document.querySelector(".desktop__header");
+  links = document.querySelector(".desktop__header"),
+  overlay = document.querySelector(".overlay__body"),
+  logo = document.querySelector(".mobile__header-logo"),
+  searchMobile = document.querySelector(".search__button"),
+  searchMobileContainer = document.querySelector(".search__input-container"),
+  countryMobile = document.querySelector(".country__switch-tablet");
+
+// mobile menu show / close
 menuBtn.addEventListener("click", function () {
   links.classList.toggle("active");
   body.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
 // fix / scroll body when menu is open
@@ -12,5 +19,44 @@ window.addEventListener("resize", function () {
   if (body.classList.contains("active") && links.classList.contains("active")) {
     body.classList.remove("active");
     links.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+});
+
+// open / close mobile search panel
+searchMobile.addEventListener("click", function () {
+  searchMobileContainer.classList.toggle("opened");
+});
+
+// remove mobile menu & body overlay when logo clicked
+logo.addEventListener("click", function () {
+  if (body.classList.contains("active")) {
+    body.classList.remove("active");
+    links.classList.remove("active");
+    overlay.classList.remove("active");
+    searchMobileContainer.classList.remove("opened");
+  }
+});
+
+// remove mobile search panel when logo clicked
+logo.addEventListener("click", function () {
+  if (searchMobileContainer.classList.contains("opened")) {
+    searchMobileContainer.classList.remove("opened");
+  }
+});
+
+// remove mobile search panel when menu clicked
+menuBtn.addEventListener("click", function () {
+  if (searchMobileContainer.classList.contains("opened")) {
+    searchMobileContainer.classList.remove("opened");
+  }
+});
+
+// remove menu when mobile search panel clicked
+searchMobile.addEventListener("click", function () {
+  if (links.classList.contains("active")) {
+    links.classList.remove("active");
+    body.classList.remove("active");
+    overlay.classList.remove("active");
   }
 });
