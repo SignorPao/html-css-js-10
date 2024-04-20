22d3f4ee90a983ebe8ad1f21b661252c855e093c
Pavel Belousov <signorpao@gmail.com>
2024-04-20 09:29:39 +0300

    navigation menu & summenu are ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,25 +3,39 @@ let menuBtn = document.querySelector(".mobile__header-burger"),
   links = document.querySelector(".desktop__header"),
   overlay = document.querySelector(".overlay__body"),
   logo = document.querySelector(".mobile__header-logo"),
+  logoDesktop = document.querySelector(".desktop__header-logo"),
   searchMobile = document.querySelector(".search__button"),
   searchMobileContainer = document.querySelector(".search__input-container"),
   countryMobile = document.querySelector(".country__switch-tablet"),
-  subnavMenu = document.querySelectorAll(".subnav");
+  subnavAccordionItems = document.querySelectorAll(".navigation__list-item");
 
 // mobile menu show / close
 menuBtn.addEventListener("click", function () {
   links.classList.toggle("active");
   body.classList.toggle("active");
   overlay.classList.toggle("active");
+  subnavAccordionItems.forEach((item) => {
+    let isShow = item.classList.contains("show-subnav");
+    if (isShow) {
+      item.classList.remove("show-subnav");
+    }
+  });
 });
 
-// fix / scroll body when menu is open
+// delete styles when window resize
 window.addEventListener("resize", function () {
   if (body.classList.contains("active") && links.classList.contains("active")) {
     body.classList.remove("active");
     links.classList.remove("active");
     overlay.classList.remove("active");
   }
+  subnavAccordionItems.forEach((item) => {
+    item.classList.remove("show-subnav");
+    let isShow = item.classList.contains("show-subnav");
+    if (!isShow) {
+      overlay.classList.remove("active");
+    }
+  });
 });
 
 // open / close mobile search panel
@@ -62,13 +76,36 @@ searchMobile.addEventListener("click", function () {
   }
 });
 
-// add / remove body overlay when desktop submenu is open
-const submenuArray = [...subnavMenu];
-console.log(submenuArray);
-for (let i = 0; i < submenuArray.length; i++) {
-  if (submenuArray[i].classList.contains("show-subnav")) {
-    overlay.classList.add("active");
-  } else {
-    overlay.classList.remove("active");
-  }
-}
+// add handlers to subnav menu
+subnavAccordionItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    let isItemOpen = item.classList.contains("show-subnav");
+    subnavAccordionItems.forEach((item) =>
+      item.classList.remove("show-subnav")
+    );
+    if (!isItemOpen) {
+      item.classList.toggle("show-subnav");
+      if (window.innerWidth > 1440) {
+        overlay.classList.add("active");
+      }
+    } else {
+      if (window.innerWidth > 1440) {
+        overlay.classList.remove("active");
+      }
+    }
+  });
+});
+
+// remove all when desktop logo clicked
+logoDesktop.addEventListener("click", function () {
+  console.log("desktop logo clicked");
+  subnavAccordionItems.forEach((item) => {
+    let isShow = item.classList.contains("show-subnav");
+    if (isShow) {
+      item.classList.remove("show-subnav");
+    }
+    if (!isShow) {
+      overlay.classList.remove("active");
+    }
+  });
+});
